2e939d02a5c0c1faf82651970410154337aebe8f
Aucante <sebastien.aucante@gmail.com>
2026-08-10 23:15:59 +0200

    Fix Login

--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -75,7 +75,7 @@ export default function Login({ status, canResetPassword }) {
                             }
                         />
                         <span className="ms-2 text-sm text-gray-600">
-                            Remember
+                            Remember me
                         </span>
                     </label>
                 </div>
