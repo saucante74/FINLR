4aa95acc4701dd56da0186abfe80c720548f2d76
Aucante <sebastien.aucante@gmail.com>
2026-08-19 16:18:06 +0200

    Refactor: remove dummy calculator engine, fail loudly if finlr-engine is missing

--- tests/Feature/Simulator/SimulationEngineBindingTest.php
+++ tests/Feature/Simulator/SimulationEngineBindingTest.php
@@ -4,7 +4,6 @@ namespace Tests\Feature\Simulator;
 
 use App\Modules\Simulator\Actions\CalculateProjectionAction;
 use App\Modules\Simulator\Contracts\SimulationEngineInterface;
-use App\Modules\Simulator\Services\DummyCalculatorEngine;
 use App\Modules\Simulator\Services\FinlrEngineAdapter;
 use Tests\TestCase;
 
@@ -15,20 +14,10 @@ class SimulationEngineBindingTest extends TestCase
         if (! class_exists('saucante74\\CalculatorEngine\\CalculatorEngine')) {
             $this->markTestSkipped('The private saucante74\\CalculatorEngine package is not installed.');
         }
-        config(['calculator.force_dummy' => false]);
 
         $engine = $this->app->make(SimulationEngineInterface::class);
 
         $this->assertInstanceOf(FinlrEngineAdapter::class, $engine);
-    }
-
-    public function test_the_dummy_engine_is_bound_when_force_dummy_is_enabled(): void
-    {
-        config(['calculator.force_dummy' => true]);
-
-        $engine = $this->app->make(SimulationEngineInterface::class);
-
-        $this->assertInstanceOf(DummyCalculatorEngine::class, $engine);
     }
 
     public function test_the_calculate_projection_action_resolves_through_the_container(): void
